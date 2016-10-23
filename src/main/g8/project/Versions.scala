f5b2b8221cb8aa5023fe5d54e5dfdaefe1451bbb
Andrés Peralta <ar.peralta.3@gmail.com>
2016-10-23 18:10:57 -0500

    ajustes de template

--- src/main/g8/project/Versions.scala
+++ src/main/g8/project/Versions.scala
@@ -9,12 +9,12 @@ object Versions {
   // Compile
   // ---------------------
 
-  val jdk: String   = "$jvm_version$"
-  val scala: String = "$scala_version$"
-  val scalaLogging: String   = "3.5.0"
-  val slf4j: String          = "1.7.21"
-  val logback: String        = "1.1.7"
-  val groovy: String         = "2.4.7"
+  val jdk: String             = "$jvm_version$"
+  val scala: String           = "$scala_version$"
+  val `scala-logging`: String = "3.5.0"
+  val slf4j: String           = "1.7.21"
+  val logback: String         = "1.1.7"
+  val `groovy-all`: String    = "2.4.7"
 
   // ---------------------
   // Test
